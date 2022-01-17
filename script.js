1c91c195d43b6a74c928977b56ef926a055a4f44
smarr2198 <smarr2198@gmail.com>
2022-01-17 10:39:35 -0600

    added rest of the rows to complete the day

--- script.js
+++ script.js
@@ -17,5 +17,12 @@ $("#currentDay").text(moment().format("MMMM Do, YYYY"));
 //grab tasks from local storage
 $("#hour9 .js-task").val(localStorage.getItem("hour9"));
 $("#hour10 .js-task").val(localStorage.getItem("hour10"));
+$("#hour11 .js-task").val(localStorage.getItem("hour11"));
+$("#hour12 .js-task").val(localStorage.getItem("hour12"));
+$("#hour1 .js-task").val(localStorage.getItem("hour1"));
+$("#hour2 .js-task").val(localStorage.getItem("hour2"));
+$("#hour3 .js-task").val(localStorage.getItem("hour3"));
+$("#hour4 .js-task").val(localStorage.getItem("hour4"));
+$("#hour5 .js-task").val(localStorage.getItem("hour5"));
 //user clicks save button
 $(".js-saveBtn").on("click", saveTask);
